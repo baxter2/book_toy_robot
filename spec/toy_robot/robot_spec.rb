1bd219142ff520169487968fa3b0a1632689e0bc
baxter2 <baxcallan@gmail.com>
2018-10-26 00:04:03 +1000

    Change instnace method #move to instance method #move_east, gives more of a relevant meaning to the problem

--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -4,12 +4,12 @@ describe ToyRobot::Robot do
   subject { ToyRobot::Robot.new(0) }
  
   it "moves 3 spaces" do
-    3.times { subject.move }
+    3.times { subject.move_east }
     expect(subject.position).to eq(3)
   end
 
   it "moves 4 spaces" do
-    4.times { subject.move }
+    4.times { subject.move_east }
     expect(subject.position).to eq(4)
   end
 end
