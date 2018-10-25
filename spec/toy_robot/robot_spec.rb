185286175f5fede0255565d9d13f177423a88cc2
baxter2 <baxcallan@gmail.com>
2018-10-26 00:07:49 +1000

    Add instance method #move_west

--- spec/toy_robot/robot_spec.rb
+++ spec/toy_robot/robot_spec.rb
@@ -3,14 +3,19 @@ require "spec_helper"
 describe ToyRobot::Robot do
   subject { ToyRobot::Robot.new(0) }
  
-  it "moves 3 spaces" do
+  it "moves 3 spaces east" do
     3.times { subject.move_east }
     expect(subject.position).to eq(3)
   end
 
-  it "moves 4 spaces" do
+  it "moves 4 spaces east" do
     4.times { subject.move_east }
     expect(subject.position).to eq(4)
+  end
+
+  it "moves 3 spaces west" do
+    3.times { subject.move_west }
+    expect(subject.position).to eq(-3)
   end
 end
 
