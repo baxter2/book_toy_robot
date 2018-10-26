501dc0f0fd5bf38826d8be99e9008624a85405d7
baxter2 <baxcallan@gmail.com>
2018-10-26 16:24:37 +1000

    Refactor instance method #turn_left

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -1,5 +1,6 @@
 module ToyRobot
   class Robot
+    DIRECTIONS = ["NORTH", "EAST", "SOUTH", "WEST"]
     attr_reader :east, :north, :direction
 
     def initialize(east = 0, north = 0, direction = "NORTH")
@@ -29,12 +30,7 @@ module ToyRobot
     end
 
     def turn_left
-      @direction = case @direction
-                   when "NORTH"    then "WEST"
-                   when "WEST"     then "SOUTH"
-                   when "SOUTH"    then "EAST"
-                   when "EAST"      then "NORTH"
-      end
+      @direction = DIRECTIONS[DIRECTIONS.index(@direction) - 1]
     end
   end
 end
