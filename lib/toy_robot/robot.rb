c6f39e532c5c7ecc6f4468375ff5b888d2e6cc50
baxter2 <baxcallan@gmail.com>
2018-10-27 13:37:33 +1000

    Add instance method #report

--- lib/toy_robot/robot.rb
+++ lib/toy_robot/robot.rb
@@ -9,6 +9,14 @@ module ToyRobot
       @direction = direction
     end
   
+    def report
+      {
+        north: north,
+        east: east,
+        direction: direction
+      }
+    end
+  
     def move_east
       @east += 1
     end
